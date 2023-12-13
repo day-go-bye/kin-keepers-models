6f463d18cc4ce7ee003ffd2812e57ea5e9a6bb28
day-go-bye <109034698+day-go-bye@users.noreply.github.com>
2023-12-12 22:02:31 -0500

    Update dementia_model_test.py

--- dementia_model_test.py
+++ dementia_model_test.py
@@ -1,6 +1,87 @@
 from keras.models import load_model
 import json
 import numpy as np
+
+
+DATA_PATH = "data-test.json"
+
+def load_data(data_path):
+    """Loads training dataset from json file.
+    
+        :param data_path (str): Path to json file containing data
+        :return X (ndarray): Inputs
+        :return y (ndarray): Targets
+    """
+
+    with open(data_path, "r") as fp:
+        data = json.load(fp)
+
+    # convert lists into numpy arrays
+    X = np.array(data["mfcc"])
+    y = np.array(data["labels"])
+
+    # print("Data successfully loaded!")
+
+    return X, y
+
+def predict(model, X, y):
+
+    X = X[np.newaxis, ...]
+
+    # prediction = [ [0.1, 0.2, ...] ] values of all different genre scores
+    prediction = model.predict(X) # X -> (1, 130, 13, 1)
+
+    # exstract index with max value
+    predicted_index = np.argmax(prediction, axis=1) # [4]
+    print("Expected index: {}, Predicted index: {}".format(y, predicted_index))
+    #if y == predicted_index:
+    #    print("prediction correct!")
+
+    return predicted_index
+    
+
+def predictions():
+    X, y = load_data(DATA_PATH)
+    test_amount = len(X)
+    correct_predictions = 0
+    test_count = 0
+    
+    for i in range(test_amount): 
+        
+        X, y = load_data(DATA_PATH)
+
+        # make a prediction on a sample
+        print("X index was " + str(test_count))
+        X = X[test_count] # random sample from data
+        y = y[test_count]
+        predicted_index = predict(model, X, y)
+        if y == predicted_index:
+            correct_predictions += 1
+        test_count +=1
+        percentage_correct = correct_predictions / test_count
+        print("percent accuracy so far : " + str(percentage_correct*100) + "%")
+        
+
+    percentage_correct = correct_predictions / test_amount
+
+    print("test accuracy was : " + str(percentage_correct*100) + "%")
+        
+        
+
+
+
+if __name__ == "__main__":
+
+    # To load the model later for predictions
+    model = load_model("dementia_model.keras")
+
+    predictions()
+    
+    
+
+    from keras.models import load_model
+import json
+import numpy as np
 import random
 
 
